2d20a8a5785b6c0cc4a4bed1faf786761cdd08af
shiv2130 <shivanahuomili@gmail.com>
2024-09-29 12:06:58 +0530

    Beginner level javascript

---
+++ main.js
@@ -0,0 +1,36 @@
+//commnent
+console.log("HEllo World!");
+//variables 
+// let name =  "Shivanshu";
+// console.log(name);
+
+//variables can't be reserved keywords
+//should be meaningful
+//cannot start with a number 
+//cannot contain a space or hyphen
+//are case sensitive
+let firstName, lastName;
+const interestRate = 0.3;
+//interestRate = 1;
+console.log(interestRate);
+//Primitive types consists of number, string, boolean , null, undefined
+//Dyanamic-> JavaScript is dyanamic language
+// typeof name
+//reference type
+let person = {
+    name: " Jhon",
+    age: 13,
+
+
+};//-> the curly braces are object literals 
+//dot notation 
+person.name = "Shivanshu";
+
+//bracket notation
+person['name'] = "Mosh";
+console.log(person);
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = "Green";
+console.log(selectedColors);
+//techincally an array is an object
